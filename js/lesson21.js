a7b8c5e9a38020b969829bdf262e3225895f0b08
Gabriel Oliveira Pinto <157530699+gaboliveira-alt@users.noreply.github.com>
2026-05-14 23:44:21 -0300

    feat: new lessons

---
+++ js/lesson21.js
@@ -0,0 +1,5 @@
+let age = 19;
+
+console.log(age >= 18 ? "Voce pode dirigir papai" : "Não pode papai");
+
+console.log(age <= 19 ? "Voce não pode dirigir" : "Voce pode papai");
---
+++ js/lesson22.js
@@ -0,0 +1,9 @@
+let hour = 12;
+
+if (hour <= 12) {
+    console.log("Ohayo");
+    console.log("Gosaimasu");
+}
+
+if (hour <= 12)
+    console.log("Receba")
