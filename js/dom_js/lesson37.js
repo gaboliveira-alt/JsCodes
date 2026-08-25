8ca8b490f95cf681c904ebd58ca29182b6ced395
Gabriel Oliveira Pinto <157530699+gaboliveira-alt@users.noreply.github.com>
2026-08-25 15:55:55 -0300

    feat: new lessons

---
+++ js/dom_js/lesson37.js
@@ -0,0 +1,3 @@
+document.getElementsByClassName("minhaClasse")[0].innerHTML = "<p>bora bill receba";
+document.getElementsByClassName("minhaClasse")[1].innerHTML = "<p>bora bill receba";
+document.getElementsByClassName("minhaClasse")[2].innerHTML = "<p>bora bill receba";
---
+++ js/dom_js/lesson38.js
@@ -0,0 +1,2 @@
+document.getElementsByTagName("div")[0].innerHTML = "Elemento por tagname";
+document.getElementsByTagName("p")[1].innerHTML = "Elemento por tagname2";
