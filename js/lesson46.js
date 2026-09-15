abfb95a285d869940e4b92f8b1f3f497e4d97a47
Gabriel Oliveira Pinto <157530699+gaboliveira-alt@users.noreply.github.com>
2026-09-14 22:57:33 -0300

    feat: new lessons

---
+++ js/lesson46.js
@@ -0,0 +1,7 @@
+const newArray = new Array();
+console.log(newArray);
+console.log("Rodrigo".length);
+
+const availableArray = new Array(10);
+console.log(availableArray);
+console.log(availableArray.length);
---
+++ js/lesson47.js
@@ -0,0 +1,9 @@
+let fruitsArray = ["Morango", "Orange", "Watermeloon", "Hadrossauro", "Carnotauro"];
+console.log(fruitsArray);
+
+console.log(fruitsArray.length);
+
+console.log(fruitsArray[0]);
+console.log(fruitsArray[4]);
+
+console.log(fruitsArray[fruitsArray.length - 1]);
